ae2697ad8e371d7337d4377ecf412f9e90c73a81
Chronosbabu <ton-email-github@gmail.com>
2025-10-03 16:00:26 +0200

    K

--- serveurbabu.py
+++ serveurbabu.py
@@ -1,4 +1,3 @@
-## fichier 1
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session, abort, jsonify
 from flask_socketio import SocketIO, emit, join_room
 import os, json, hashlib
